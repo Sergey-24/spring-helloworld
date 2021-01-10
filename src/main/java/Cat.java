ba66f85dacc79a6201d98d4894d028c63a286503
Sergey-24 <serg24061986@yandex.ru>
2021-01-10 21:43:54 +0300

    init2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+
+    private String massage;
+
+    public Cat() {
+    }
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+}
